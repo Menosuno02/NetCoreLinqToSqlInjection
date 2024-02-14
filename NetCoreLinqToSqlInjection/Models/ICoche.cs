87700548cea02b171b365eb1bcca3ea9b85912a3
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-14 13:33:07 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Models/ICoche.cs
@@ -0,0 +1,16 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public interface ICoche
+    {
+        // Las interfaces no tienen ámbito, solamente
+        // los métodos y propiedades que debe tener un objeto
+
+        string Marca { get; set; }
+        string Modelo { get; set; }
+        string Imagen { get; set; }
+        int Velocidad { get; set; }
+        int VelocidadMaxima { get; set; }
+        int Acelerar();
+        int Frenar();
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Repositories/IRepositoryDoctores.cs
@@ -0,0 +1,10 @@
+﻿using NetCoreLinqToSqlInjection.Models;
+
+namespace NetCoreLinqToSqlInjection.Repositories
+{
+    public interface IRepositoryDoctores
+    {
+        List<Doctor> GetDoctores();
+        void InsertDoctor(int id, string apellido, string especialidad, int salario, int idHospital);
+    }
+}
